e9d9db530302666bd9c17128628a47c7fe7314ee
linuxKim <slasnrndu@gmail.com>
2021-08-18 22:54:46 +0900

    URL Routing처리 및 static 디렉토리 설정

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ var app = express();
 app.listen(3000, function() {
     console.log("start! express server on port 3000");
 });
+
+app.use(express.static('public'))
+
+// url routing
+app.get('/', function(req, res) {
+    res.sendFile(__dirname+"/public/main.html");
+});

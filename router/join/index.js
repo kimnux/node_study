cd091ede61061fa0d13207339047b4924f15ebc2
linuxKim <slasnrndu@gmail.com>
2021-08-27 23:01:26 +0900

    회원가입 추가

--- router/join/index.js
+++ router/join/index.js
@@ -16,7 +16,19 @@ router.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'../../public/join.html'));
 });
 
+router.post('/', function(req, res){
+    let body = req.body;
+    let email = body.email;
+    let name = body.name;
+    let password = body.password;
 
+    console.log({email,name,password});
+
+    let query = connection.query('insert into user (email,name,pwd) values ("'+ email + '","'+ name + '","' + password + '")', function(err,rows) {
+        if(err) {throw err;}
+        console.log('ok db insert');
+    });
+});
 
 
 module.exports = router;
